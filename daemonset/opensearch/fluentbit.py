29f80be9638b4bba23e07089ebc81f93552be7ff
Jelizaveta Lemeševa <jelizaveta.lemeseva@vinted.com>
2024-08-01 16:31:06 +0200

    Update opensearch mapping and python script

--- daemonset/opensearch/fluentbit.py
+++ daemonset/opensearch/fluentbit.py
@@ -13,25 +13,19 @@ client = OpenSearch(
     ssl_show_warn = False
 )
 
-client.indices.refresh(index='job_log')
-
-# search for all documents in the 'movies' index
+# search for all job logs
 response = client.search(index='job_log',size=50)
 
-for hit in response['hits']['hits']:
-    print(hit['_source']['kubernetes']['labels']['job-name'])
-
-# extract the count of hits from the response
 hits_count = response['hits']['total']['value']
+print("Total Job Log Hits: ", hits_count)
 
-# print the count of hits
-print("Total Hits: ", hits_count)
+first_job = response['hits']['hits'][0]['_source']['kubernetes']['labels']['job-name']
 
-reana_job = "reana-run-job-0b6b2ef1-1350-46bb-9338-8c3892e5b1ad"
+# search for logs of a specific job
 query = {
     "query": {
         "match": {
-            "kubernetes.labels.job-name": reana_job
+            "kubernetes.labels.job-name": first_job
         }
     },
     "sort": [
@@ -40,21 +34,46 @@ query = {
                 "order": "desc"
             }
         }
-    ],
-    "explain": True
+    ]
 }
 
-# search for documents in the 'movies' index with the given query
 response = client.search(index='job_log', body=query, size=50)
-
-# extract the count of hits from the response
 hits_count = response['hits']['total']['value']
+print("Hits for Job ID", first_job, ":", hits_count)
 
-# print the count of hits
-print("Total Hits: ", hits_count)
-
-if hits_count != 0:
-    print(response['hits']['hits'][0])
-
+print("Logs for Job ID", first_job, ":")
 for hit in response['hits']['hits']:
     print(hit['_source']['kubernetes']['labels']['job-name'], hit['_source']['time'], hit['_source']['log'])
+
+# search for all workflow logs
+response = client.search(index='workflow_log',size=5)
+
+hits_count = response['hits']['total']['value']
+print("Total Workflow Log Hits: ", hits_count)
+
+first_wf = response['hits']['hits'][0]['_source']['kubernetes']['labels']['reana-run-batch-workflow-uuid']
+
+# search workflow logs for a specific workflow
+query = {
+    "query": {
+        "match": {
+            "kubernetes.labels.reana-run-batch-workflow-uuid": first_wf
+        }
+    },
+    "sort": [
+        {
+            "time": {
+                "order": "desc"
+            }
+        }
+    ]
+}
+
+response = client.search(index='workflow_log', body=query, size=50)
+hits_count = response['hits']['total']['value']
+print("Hits for Workflow ID", first_wf, ":", hits_count)
+
+print("Logs for Workflow ID", first_wf, ":")
+
+for hit in response['hits']['hits']:
+    print(hit['_source']['kubernetes']['labels']['reana-run-batch-workflow-uuid'], hit['_source']['time'], hit['_source']['log'])
